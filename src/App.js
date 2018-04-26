0db9bf26ae30a24e43ae7b6c091841ba68972b56
lewisbrotchie <noble.ninja@gmail.com>
2018-04-26 14:41:19 +0100

    days of the week implemented crudely

--- src/App.js
+++ src/App.js
@@ -5,15 +5,26 @@ import "./App.css";
 
 class App extends Component {
   render() {
+    const d = new Date();
+    const weekday = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+
     return (
       <div className="App">
-        <DayCard day="Monday" />
-        <DayCard day="Tuesday" />
-        <DayCard day="Wednesday" />
-        <DayCard day="Thursday" />
-        <DayCard day="Friday" />
-        <DayCard day="Saturday" />
-        <DayCard day="Sunday" />
+        <DayCard day={weekday[d.getDay()]} />
+        <DayCard day={weekday[d.getDay() + 1]} />
+        <DayCard day={weekday[d.getDay() + 2]} />
+        <DayCard day={weekday[d.getDay() + 3]} />
+        <DayCard day={weekday[d.getDay() + 4]} />
+        <DayCard day={weekday[d.getDay() + 5]} />
+        <DayCard day={weekday[d.getDay() + 6]} />
       </div>
     );
   }
