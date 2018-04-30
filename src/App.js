2d26105478e1c1c85a5d0e4ffce46386951e6242
lewisbrotchie <noble.ninja@gmail.com>
2018-04-30 17:13:50 +0100

    started refactoring to use arrays properly

--- src/App.js
+++ src/App.js
@@ -10,92 +10,65 @@ import Logo from "./logo.svg";
 import "./App.css";
 
 const d = new moment(new Date());
+const today = new moment(new Date());
 const url = `https://api.openweathermap.org/data/2.5/forecast?q=London,uk&mode=JSON&units=metric&APPID=${api}`;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tempHigh: [0, 0, 0, 0, 0, 0],
-      tempLow: [0, 0, 0, 0, 0, 0]
+      cards: [0, 0, 0, 0],
+      tempHigh: [0, 0, 0, 0, 0],
+      tempLow: [0, 0, 0, 0, 0]
     };
   }
 
   componentDidMount() {
+    let dataIndex = 0;
     axios
       .get(url)
       .then(response => {
         console.log(response);
-        this.state.tempHigh.forEach((element, index) => {
-          let newTemps = this.state.tempHigh.slice();
-          newTemps[index] = Math.ceil(response.data.list[index].main.temp_max);
-          this.setState({
-            tempHigh: newTemps
-          });
-          newTemps[index] = Math.ceil(response.data.list[index].main.temp_min);
-          this.setState({
-            tempLow: newTemps
-          });
+        const filtered = response.data.list.filter((item, index) => {
+          return (
+            response.data.list[index].id ===
+            (response.data.list[index].id / 8) % 0
+          );
         });
+        console.log(filtered);
+        const tempHigh = this.state.tempHigh.map((temp, index) => {
+          return response.data.list[dataIndex].main.temp_max;
+        });
+        console.log(tempHigh);
       })
       .catch(error => {
         console.log(error.toString());
       });
   }
   render() {
+    //tomorrow + onwards
+    const cards = this.state.cards.map((card, index) => {
+      return (
+        <DayCard
+          day={d
+            .add(1, "days")
+            .toString()
+            .substring(0, 4)}
+          icon={Logo}
+          tempHigh={"hi: " + this.state.tempHigh[index]}
+          tempLow={"lo: " + this.state.tempLow[index]}
+        />
+      );
+    });
     return (
       <div className="App">
         <DayCard
-          day={d.toString().substring(0, 4)}
+          day={today.toString().substring(0, 4) + "(Today)"}
           icon={Logo}
-          tempHigh={"hi: " + this.state.tempHigh}
-          tempLow={"low: " + this.state.tempLow}
+          tempHigh={"hi: " + this.state.tempHigh[0]}
+          tempLow={"low: " + this.state.tempLow[0]}
         />
-        <DayCard
-          day={d
-            .add(1, "days")
-            .toString()
-            .substring(0, 4)}
-          icon={Logo}
-          tempHigh="25"
-          tempLow="13"
-        />
-        <DayCard
-          day={d
-            .add(1, "days")
-            .toString()
-            .substring(0, 4)}
-          icon={Logo}
-          tempHigh="26"
-          tempLow="14"
-        />
-        <DayCard
-          day={d
-            .add(1, "days")
-            .toString()
-            .substring(0, 4)}
-          icon={Logo}
-          tempHigh="27"
-          tempLow="15"
-        />
-        <DayCard
-          day={d
-            .add(1, "days")
-            .toString()
-            .substring(0, 4)}
-          icon={Logo}
-          tempHigh="28"
-          tempLow="16"
-        />
-        <DayCard
-          day={d
-            .add(1, "days")
-            .toString()
-            .substring(0, 4)}
-          icon={Logo}
-          tempHigh="29"
-          tempLow="17"
-        />
+        {cards}
       </div>
     );
   }
