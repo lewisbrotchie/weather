4e48a70961e38c3125b6e4b177cfbfe1ee16c406
lewisbrotchie <noble.ninja@gmail.com>
2018-05-03 16:12:45 +0100

    geolocation implemented with title

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      cityName: " ",
+      located: false,
+      latitude: 0,
+      longitude: 0,
       icon: [],
       cards: [0, 0, 0, 0, 0],
       tempHigh: []
@@ -22,8 +26,12 @@ class App extends Component {
   }
 
   componentWillMount() {
+    const locatedURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${
+      this.state.latitude
+    }&lon=${this.state.longitude}&mode=JSON&units=metric&APPID=${api}`;
+
     axios
-      .get(url)
+      .get(this.state.located ? locatedURL : url)
       .then(response => {
         console.log(response);
         const filtered = response.data.list.filter((element, index) => {
@@ -38,19 +46,51 @@ class App extends Component {
           );
         });
         this.setState({
+          cityName: response.data.city.name,
           tempHigh: arr,
           icon: icons
         });
       })
+      .then(this.getLocation())
       .catch(error => {
         console.log(error.toString());
       });
+  }
+  getLocation() {
+    navigator.geolocation.getCurrentPosition(
+      pos => {
+        this.setState({
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude,
+          located: true
+        });
+        console.log(pos.coords.latitude);
+        console.log(pos.coords.longitude);
+      },
+      err => {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      },
+      {
+        enableHighAccuracy: true
+      }
+    );
   }
 
   render() {
     return (
       <div>
-        <h1 align="center">London Weather</h1>
+        <h1 align="center">{this.state.cityName}</h1>
+        <div
+          style={{ display: "flex", justifyContent: "center", margin: "2%" }}
+        >
+          <button
+            onClick={() => {
+              this.componentWillMount();
+            }}
+          >
+            Locate me, bitch
+          </button>
+        </div>
         <div className="Cards">
           {this.state.cards.map((card, index) => {
             return (
